3b0de102a3a4cd355cee5c66ed08c6e246830e29
James Derwin <birdtree@Jamess-MacBook-Pro.local>
2019-03-15 12:20:16 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log(cardsInPlay)
+var cardTwo = cards[2];
+cardsInPlay.push("king");
+console.log(cardsInPlay)
+console.log()
+if (cardsInPlay.length===2) {
+  	console.log(cardsInPlay)
+}
+if (cardsInPlay[0]===cardsInPlay[1]){
+ 	alert("You found a match!") 
+}else{
+ 	alert("Sorry, try again.")
+}
